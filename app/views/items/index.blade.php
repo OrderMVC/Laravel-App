2b25761d57a83e97d388347cfc1379f21242cf11
Ryan Tablada <ryan.tablada@gmail.com>
2013-08-27 13:11:11 -0400

    Laying out items

--- app/views/items/index.blade.php
+++ app/views/items/index.blade.php
@@ -2,11 +2,24 @@
 
 @section('content')
 <div class="row">
-	<div class="xlarge primary btn">
-		{{HTML::linkRoute('items.index', 'Shop Now')}}
+@if($items)
+	<div class="grid">
+		@foreach($items as $key => $item)
+			<ul class="four_up tiles">
+				@if(class_basename($item) === 'PhysicalItem')
+					<li>
+						<img src="http://placepuppy.it/images/homepage/Afra_004.jpg" class="photo image">
+					</li>
+				@else
+					<li>
+						<img src="http://placepuppy.it/images/homepage/Beagle_puppy_6_weeks.JPG" class="photo image">
+					</li>
+				@endif
+			</ul>
+		@endforeach
 	</div>
-	<div class="xlarge primary btn">
-		{{HTML::linkRoute('orders.index', 'View Orders')}}
-	</div>
+@else
+	<h4>Yo Man! There aren't any items yet!</h4>
+@endif
 </div>
 @stop
