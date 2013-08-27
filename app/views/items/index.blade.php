01a92a7d3a08dcff35555c60837439df6760bf1f
Ryan Tablada <ryan.tablada@gmail.com>
2013-08-27 13:21:47 -0400

    Working on items.index

--- app/views/items/index.blade.php
+++ app/views/items/index.blade.php
@@ -3,23 +3,39 @@
 @section('content')
 <div class="row">
 @if($items)
-	<div class="grid">
-		@foreach($items as $key => $item)
-			<ul class="four_up tiles">
-				@if(class_basename($item) === 'PhysicalItem')
-					<li>
-						<img src="http://placepuppy.it/images/homepage/Afra_004.jpg" class="photo image">
-					</li>
-				@else
-					<li>
-						<img src="http://placepuppy.it/images/homepage/Beagle_puppy_6_weeks.JPG" class="photo image">
-					</li>
-				@endif
-			</ul>
-		@endforeach
-	</div>
+  <div class="grid">
+    @foreach($items as $key => $item)
+      <ul class="four_up tiles">
+        @if(class_basename($item) === 'PhysicalItem')
+          <li>
+            <figure>
+              <img src="http://placepuppy.it/images/homepage/Afra_004.jpg" class="photo image">
+            </figure>
+            <figcaption>
+              <div class="label info">Physical</div>
+              <p><strong>{{$item->name}}</strong></p>
+              <p>Weight: {{$item->weight}} Lbs</p>
+              <p>{{$item->stock}} Left</p>
+            </figcaption>
+          </li>
+        @else
+          <li>
+            <figure>
+              <img src="http://placepuppy.it/images/homepage/Beagle_puppy_6_weeks.JPG" class="photo image">
+            </figure>
+            <figcaption>
+              <div class="label default">Digital</div>
+              <p><strong>{{$item->name}}</strong></p>
+              <p>Size: {{$item->size}} Mbs</p>
+              <p>Format: {{$item->format}}</p>
+            </figcaption>
+          </li>
+        @endif
+      </ul>
+    @endforeach
+  </div>
 @else
-	<h4>Yo Man! There aren't any items yet!</h4>
+  <h4>Yo Man! There aren't any items yet!</h4>
 @endif
 </div>
 @stop
